70001bfaf9ec524094a3bbdf6ec9f680e6f66176
Shivam Vats <123102767+shivam37483@users.noreply.github.com>
2025-01-19 21:09:34 +0000

    Deck

---
+++ Saturn/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // Rust: Sequence ----> Python:List 
+    let seq = [1,2,3,4,5];
+
+    for n in seq {
+        print!("{}", n);
+    }
+}
+
---
+++ Saturn/target/debug/Saturn.d
@@ -0,0 +1,1 @@
+/workspaces/Elysium/Saturn/target/debug/Saturn: /workspaces/Elysium/Saturn/src/main.rs
