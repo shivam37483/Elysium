5c929ec451c144bcfdaff4fbf1a2551cbd2e6189
Shivam Vats (Nokia) <shivam.vats@nokia.com>
2025-01-21 16:20:51 +0530

    freedom

--- luther/hashmaps/src/main.rs
+++ luther/hashmaps/src/main.rs
@@ -1,13 +1,6 @@
-use std::collections::HashMap;
+// use std::collections::HashMap;
 
 fn main() {
-    let mut m = HashMap::new();
-
-    m.insert(3,4);
-
-    match m.get(&5) {
-        Some(v) => println!("yrreeee"),
-        None => println!("trash"),
-    };
+    // let s = "cre";
+    println!("PAet");
 }
-
