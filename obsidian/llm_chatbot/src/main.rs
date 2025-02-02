6f7c81b106b75ea295e9d435d15976eb261d8559
Shivam Vats (Nokia) <shivam.vats@nokia.com>
2025-02-02 15:17:31 +0530

    quintess

--- obsidian/llm_chatbot/src/main.rs
+++ obsidian/llm_chatbot/src/main.rs
@@ -3,22 +3,23 @@ use llm_chatbot::run_chat_loop;
 // reqwest for making HTTP requests
 use reqwest::Client;
 
-use std::env;
+// use std::env;
 
 
 #[tokio::main]
 async  fn main() -> Result<(), reqwest::Error> {
     let client = Client::new();
 
-    let api_key = match env::var("") {
-        Ok(key) => key,
-        Err(_) => {
-            println!("Key not working");
-            String::from("error from the key")
-        }
-    };
+    // let api_key2 = match env::var("") {
+    //     Ok(key) => key,
+    //     Err(_) => {
+    //         println!("Key not working");
+    //         String::from("error from the key")
+    //     }
+    // };
 
-    let url = "";
+    let url = "https://openrouter.ai/api/v1";
+    let api_key = "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";
 
     run_chat_loop(&client, &api_key, url).await?;
     
---
+++ obsidian/llm_chatbot/target/debug/libllm_chatbot.d
@@ -0,0 +1,1 @@
+C:\Elysium\obsidian\llm_chatbot\target\debug\libllm_chatbot.rlib: C:\Elysium\obsidian\llm_chatbot\src\lib.rs
